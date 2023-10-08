1d74c7eb2a0c05af9c09a7e552da92c117aa23c3
Amit Mandal <amitmandalbm99@gmail.com>
2023-10-08 23:57:19 +0530

    DSA in java Day 9

--- DataStructures/src/com/amit/arrays/AddtoArrayFormofInteger.java
+++ DataStructures/src/com/amit/arrays/AddtoArrayFormofInteger.java
@@ -1,25 +1,45 @@
 package com.amit.arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AddtoArrayFormofInteger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] num = {1,2,0,0};
+		int k = 34;
+		
+		for(int list: addToArrayForm(num,k)) {
+			System.out.print(list +" ");
+		}
+		
 
 	}
 	
-    public List<Integer> addToArrayForm(int[] num, int k) {
+    public static List<Integer> addToArrayForm(int[] num, int k) {
     	List<Integer> list = new ArrayList<>();
     	
-    	int length= num.length;
-    	int i=length-1;
+    	int i= num.length-1;
     	
     	while(i>=0 || k>0) {
     		
+    		if(i>=0) {
+    			list.add((num[i]+k) % 10);
+    			k/= 10; 
+    		}
+    		
+    		else {
+    			list.add(k%10);
+    			k/= 10;
+    		}
+    		
+    		i--;
     	}
     	
+    	Collections.reverse(list);
     	
     	return list;
         
